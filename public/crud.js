b0c62dcc6a61e137faa1cd5a86270dcb1d02f1c4
Laura Bassani <bassanilaurarita@gmail.com>
2020-11-22 08:57:06 +0100

    add image and change style

--- public/crud.js
+++ public/crud.js
@@ -1,6 +1,6 @@
 'use strict'
+    
     let movieCard = document.getElementById("movies");
-    movieCard.style.cssText = "display: flex";
     let movieList= [];
 
     fetch('http://localhost:3000/peliculas')
@@ -13,30 +13,44 @@
      function createMovie(){
          movieList.forEach(movie => {
              const cardMovie = document.createElement('div');
-             cardMovie.style.cssText = "border: 2px black solid;width:15rem;padding: 1em; margin: 1em";
+             cardMovie.className = "card border";
+             cardMovie.style.cssText = "width: 18rem;";
+
+            const body = document.createElement('div');
+            body.className = "card-body";
+            body.style.cssText = "align-self: center"
+
              const idCard = document.createElement('p');
              idCard.innerHTML = movie.id;
-             idCard.style.cssText ="text-align:end; margin: 0.2vw"
-             cardMovie.appendChild(idCard);
+             idCard.className = "card-title"
+             body.appendChild(idCard);
+             cardMovie.appendChild(body);
              
-             const nombreCard = document.createElement('p');
+             const nombreCard = document.createElement('h5');
              nombreCard.innerHTML = movie.nombre;
-             nombreCard.style.cssText = "font-size:2vw; color:cornflowerblue;font-style:oblique;text-align:center"
-             cardMovie.appendChild(nombreCard);
+             nombreCard.className = "card-header";
+             body.appendChild(nombreCard);
+             cardMovie.appendChild(body);
 
              const directorCard = document.createElement('p');
              directorCard.innerHTML = movie.director;
-             directorCard.style.cssText = "font-size:1.2vw; color:grey;text-align:center"
-             cardMovie.appendChild(directorCard);
+             directorCard.className = "card-text";
+             body.appendChild(directorCard);
+             cardMovie.appendChild(body);
 
-             const imagenCard = document.getElementById("img");
+             const imagenCard = document.createElement("img");
+             imagenCard.src = movie.image;
              imagenCard.innerHTML = movie.image;
-             directorCard.appendChild(imagenCard);
+             imagenCard.className = "card-img-top";
+             imagenCard.style.cssText = "width:10em"
+             body.appendChild(imagenCard);
+             cardMovie.appendChild(body);
 
-             const clasificacionCard = document.createElement('p');
+             const clasificacionCard = document.createElement('div');
              clasificacionCard.innerHTML = movie.clasificacion;
-             clasificacionCard.style.cssText = "font-size:0.8vw; color:gray; text-align:center"
-             cardMovie.appendChild( clasificacionCard);
+             clasificacionCard.className = "card-footer";
+             body.appendChild(clasificacionCard);
+             cardMovie.appendChild(body);
 
              movieCard.appendChild(cardMovie);
             console.log(movie);
@@ -45,38 +59,7 @@
      }
      
     
-// let movieList = [];
-// fetch('http://localhost:3000/peliculas')
-// .then(res => res.json())
-// .then(movies => {
-//     movieList = movies;
-//     createTable();
-    
-// })
-// function createTable() {
-//     movieList.forEach( movie => {
-//         const rowMovie = document.createElement('tr');
-//         const idMovie = document.createElement('td');
-//         idMovie.innerHTML = movie.id;
-//         rowMovie.appendChild(idMovie);
 
-        
-//         const titleMovie = document.createElement('td');
-//         titleMovie.innerHTML = movie.nombre;
-//         rowMovie.appendChild(titleMovie);
-
-//         const directorMovie = document.createElement('td');
-//         directorMovie.innerHTML = movie.director;
-//         rowMovie.appendChild(directorMovie);
-
-//         const clasMovie = document.createElement('td');
-//         clasMovie.innerHTML = movie.clasificacion;
-//         rowMovie.appendChild(clasMovie);
-//         moviesTable.appendChild(rowMovie);
-//         console.log(movie);
-        
-//     })
-// }
 
 
 
